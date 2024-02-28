7f9aed4917f444991a42d36d91def8cb3d8a6f02
Shashank Malik <161154074+shashank2203malik@users.noreply.github.com>
2024-02-29 01:09:07 +0530

    Update test.js

--- array-strings/find-pivot-index/test.js
+++ array-strings/find-pivot-index/test.js
@@ -2,12 +2,12 @@ const {pivotIndex} = require ("./solution.js");
 
 function driver () {
     const testCase1 = pivotIndex([1,7,3,6,5,6]);
-	const testCase2 = pivotIndex([1,2,3]);
-	const testCase3 = pivotIndex([2,1,-1]);
+    const testCase2 = pivotIndex([1,2,3]);
+    const testCase3 = pivotIndex([2,1,-1]);
 
     console.log(testCase1);
-	console.log(testCase2);
-	console.log(testCase3);
+    console.log(testCase2);
+    console.log(testCase3);
 }
 
 driver ();
